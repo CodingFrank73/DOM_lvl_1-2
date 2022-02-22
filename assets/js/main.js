0a9e248b7a6dd552aa5c50ed2694c79ab1dbd638
Frank <codingfrank73@gmail.com>
2022-02-22 15:12:54 +0100

    ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+// ------------------- Aufgabe 1_2 ----------------------
 function myFunction() {
     let a = document.getElementsByClassName("example")
 
@@ -25,3 +25,26 @@ function myFunction() {
         }
     }
 }
+
+// ------------------- Aufgabe 2_3 ----------------------
+let clickMe = document.getElementById('navChange');
+
+clickMe.addEventListener('click', () => {
+    document.getElementsByTagName('a')[0].style.backgroundColor = "#f6c89f";
+    document.getElementsByTagName('a')[1].style.backgroundColor = "#ffe7d1";
+    document.getElementsByTagName('a')[2].style.backgroundColor = "#4b8e8d";
+    document.getElementsByTagName('a')[3].style.backgroundColor = "#396362";
+})
+
+// ------------------- Aufgabe 2_4 ----------------------
+let btn = document.querySelector('#button');
+let chk = document.querySelector('#farbeAuswahlen');
+let bgColor;
+
+btn.addEventListener('click', () => {
+    document.getElementsByTagName('form')[0].style.backgroundColor = bgColor;
+});
+
+chk.addEventListener('change', () => {
+    bgColor = chk.value.replace(/ /g, "");
+})
